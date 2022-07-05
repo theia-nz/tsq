1593047819106a0a45bf3ca5022da2ceae5c5ddf
Kevin Lim <zzkevinlim@gmail.com>
2022-07-05 15:12:57 +1200

    202207051512

--- resources/views/pages/pageHome.blade.php
+++ resources/views/pages/pageHome.blade.php
@@ -3,6 +3,14 @@
 @section('title', $repo->title)
 
 @section('content')
+
+<div class="flex items-center bg-primary-grey cursor-pointer absolute inset-0" x-data="{ loadingScreen: true }" x-cloak
+	x-show="loadingScreen" x-transition:enter="transition-all" x-transition:enter-start="opacity-0"
+	x-transition:enter-end="opacity-100" x-transition:leave="transition-all duration-1000"
+	x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" x-on:click="loadingScreen = false">
+	<img class="object-cover object-center w-full" src="{{ $repo->image('image_a') }}" alt="">
+</div>
+
 <div class="container mx-auto px-4">
 	<div>{{ $repo->title }}</div>
 </div>
