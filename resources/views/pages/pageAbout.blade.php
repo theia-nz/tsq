3ae4db920cc884977a35e40a73f15e3300b6c929
Kevin Lim <zzkevinlim@gmail.com>
2022-07-06 12:02:31 +1200

    202207061202

--- resources/views/pages/pageAbout.blade.php
+++ resources/views/pages/pageAbout.blade.php
@@ -3,8 +3,26 @@
 @section('title', $repo->title)
 
 @section('content')
-<div class="container mx-auto px-4">
-	<div>{{ $repo->title }}</div>
+<div class="relative">
+	<div class="container mx-auto px-4">
+		<div class="grid grid-cols-12 min-h-[calc(90vh-110px)]">
+			<div class="col-span-3 flex items-end">
+				<div class="font-primary font-extralight uppercase text-[36px] text-primary-grey">
+					{{ $repo->title }}
+				</div>
+			</div>
+			<div class="col-span-3 flex items-center">
+				<div class="font-primary font-light tracking-wider text-[16px] text-primary-grey">
+					{!! nl2br($repo->description) !!}
+				</div>
+			</div>
+			<div class="col-span-6">
+				<div class="absolute w-[100vw] left-0">
+					<img class="absolute right-0 max-w-[45vw]" src="{{ $repo->image('image') }}" alt="">
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 @endsection
 
