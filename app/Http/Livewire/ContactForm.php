c0998081a7040456e98c36bdf2c01b853f479605
Kevin Lim <zzkevinlim@gmail.com>
2022-07-08 14:52:06 +1200

    202207081452

--- app/Http/Livewire/ContactForm.php
+++ app/Http/Livewire/ContactForm.php
@@ -2,10 +2,91 @@
 
 namespace App\Http\Livewire;
 
+use Exception;
 use Livewire\Component;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Mail;
+use App\Repositories\FormContactRepository;
 
 class ContactForm extends Component
 {
+    public $repo;
+
+    public $name;
+    public $phone;
+    public $email;
+    public $message;
+    public $google_recaptcha_token;
+
+    protected $listeners = ['setGoogleRecaptchaToken'];
+
+    public function boot(FormContactRepository $repo)
+    {
+        $this->repo = $repo->first();
+    }
+
+    public function setGoogleRecaptchaToken($token)
+    {
+        $this->google_recaptcha_token = $token;
+
+        $this->submit();
+    }
+
+    public function submit()
+    {
+        $data = $this->validate(
+            [
+                'name' => 'required',
+                'phone' => 'required',
+                'email' => 'required|email:rfc',
+                'message' => 'required',
+                'google_recaptcha_token' => [
+                    function ($attribute, $value, $fail) {
+                        $response = Http::post('https://www.google.com/recaptcha/api/siteverify?secret=' . env('GOOGLE_RECAPTCHA_SECRET_KEY') . '&response=' . $value)->json();
+
+                        if ($response['success'] && ($response['score'] < 0.9)) {
+                            $fail($this->repo->error_recaptcha);
+                        }
+                    },
+                ],
+            ],
+            [
+                'name.required' => $this->repo->error_name,
+                'phone.required' => $this->repo->error_phone,
+                'email.required' => $this->repo->error_email_1,
+                'email.email' => $this->repo->error_email_2,
+                'message.required' => $this->repo->error_message,
+            ],
+        );
+
+        try {
+            $message = "{$this->repo->label_name}: {$data['name']}
+{$this->repo->label_phone}: {$data['phone']}
+{$this->repo->label_email}: {$data['email']}
+{$this->repo->label_message}: {$data['message']}";
+
+            foreach ($this->repo->recipients as $recipient) {
+                Mail::raw($message, function ($mailable) use ($recipient) {
+                    $mailable->to($recipient['email'][config('app.locale')], $recipient['name'][config('app.locale')])->subject($this->repo->title);
+                });
+            }
+
+            $this->reset([
+                'name',
+                'phone',
+                'email',
+                'message',
+                'google_recaptcha_token',
+            ]);
+
+            session()->flash('message_success', $this->repo->message_success);
+        } catch (Exception $e) {
+            Log::error($e);
+            session()->flash('message_failure', $this->repo->message_failure);
+        }
+    }
+
     public function render()
     {
         return view('livewire.contact-form');
--- resources/views/livewire/contact-form.blade.php
+++ resources/views/livewire/contact-form.blade.php
@@ -1,7 +1,105 @@
-<div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum tempora similique dolore consequatur laborum
-    impedit vitae, corporis in maxime ipsam molestiae ab qui blanditiis totam dolor cumque minus quibusdam dolorem
-    dolorum repellat officia. Numquam fugit nam modi, quasi dolorum autem architecto earum beatae facere, dignissimos
-    officiis nulla ratione illo fugiat similique quia tenetur sed commodi corporis quos, quisquam consequuntur? Alias
-    eos, esse sed facilis est qui, blanditiis officiis assumenda voluptate odio hic cum amet, atque suscipit magnam.
-    Quam expedita consequatur necessitatibus incidunt, dolore ipsam, saepe eveniet earum quae dolorem quas? Dicta quos
-    quas, beatae totam adipisci libero? Consequuntur, ipsa quo.</div>
+<div>
+    <form wire:submit.prevent="$emit('getGoogleRecaptchaToken')">
+        <div class="grid grid-cols-12 gap-y-[5px] gap-x-[5px]">
+            <div class="col-span-12">
+                <div class="font-primary font-medium text-[18px] text-primary-grey mb-[10px]">
+                    {{ $repo->form_title }}
+                </div>
+            </div>
+            <div class="col-span-12 lg:col-span-3">
+                <label class="font-primary font-light tracking-wider text-[16px] text-primary-grey">
+                    {{ $repo->label_name }}
+                </label>
+            </div>
+            <div class="col-span-12 lg:col-span-9">
+                <input wire:model.defer="name"
+                    class="bg-tertiary-grey-100 w-full border-0 transition-all focus:ring-primary-grey focus:ring-2 focus:ring-offset-2"
+                    type="text">
+                <div class="h-[20px]">
+                    @error('name')
+                    <small class="font-primary font-bold text-red-500">{{ $message }}</small>
+                    @enderror
+                </div>
+            </div>
+            <div class="col-span-12 lg:col-span-3">
+                <label class="font-primary font-light tracking-wider text-[16px] text-primary-grey">
+                    {{ $repo->label_phone }}
+                </label>
+            </div>
+            <div class="col-span-12 lg:col-span-9">
+                <input wire:model.defer="phone"
+                    class="bg-tertiary-grey-100 w-full border-0 transition-all focus:ring-primary-grey focus:ring-2 focus:ring-offset-2"
+                    type="text">
+                <div class="h-[20px]">
+                    @error('phone')
+                    <small class="font-primary font-bold text-red-500">{{ $message }}</small>
+                    @enderror
+                </div>
+            </div>
+            <div class="col-span-12 lg:col-span-3">
+                <label class="font-primary font-light tracking-wider text-[16px] text-primary-grey">
+                    {{ $repo->label_email }}
+                </label>
+            </div>
+            <div class="col-span-12 lg:col-span-9">
+                <input wire:model.defer="email"
+                    class="bg-tertiary-grey-100 w-full border-0 transition-all focus:ring-primary-grey focus:ring-2 focus:ring-offset-2"
+                    type="text">
+                <div class="h-[20px]">
+                    @error('email')
+                    <small class="font-primary font-bold text-red-500">{{ $message }}</small>
+                    @enderror
+                </div>
+            </div>
+            <div class="col-span-12 lg:col-span-3">
+                <label class="font-primary font-light tracking-wider text-[16px] text-primary-grey">
+                    {{ $repo->label_message }}
+                </label>
+            </div>
+            <div class="col-span-12 lg:col-span-9">
+                <textarea wire:model.defer="message"
+                    class="bg-tertiary-grey-100 w-full border-0 transition-all focus:ring-primary-grey focus:ring-2 focus:ring-offset-2"
+                    rows=3>
+                </textarea>
+                <div class="h-[20px]">
+                    @error('message')
+                    <small class="font-primary font-bold text-red-500">{{ $message }}</small>
+                    @enderror
+                </div>
+            </div>
+            <div class="col-span-12">
+                <div class="h-[20px] text-right">
+                    @error('google_recaptcha_token')
+                    <small class="font-primary font-bold text-red-500">{{ $message }}</small>
+                    @enderror
+                    @if (session()->has('message_failure'))
+                    <small class="font-primary font-bold text-red-500">{{ session('message_failure') }}</small>
+                    @endif
+                    @if (session()->has('message_success'))
+                    <small class="font-primary font-bold text-green-500">{{ session('message_success') }}</small>
+                    @endif
+                </div>
+            </div>
+            <div class="col-span-12 flex justify-end">
+                <button wire:loading.attr="disabled" type="submit"
+                    class="inline-block font-primary font-light uppercase tracking-wider text-[16px] text-primary-grey pl-[15px] border-b-[1px] border-primary-grey">
+                    {{ $repo->label_submit }}
+                </button>
+            </div>
+        </div>
+        <input wire:model.defer="google_recaptcha_token" type="hidden">
+    </form>
+    <script src="https://www.google.com/recaptcha/api.js?render={{ env('GOOGLE_RECAPTCHA_SITE_KEY') }}"></script>
+    <script>
+        document.addEventListener('livewire:load', function () {
+            Livewire.on('getGoogleRecaptchaToken', () => {
+                grecaptcha.ready(function() {
+                    grecaptcha.execute('{{ env('GOOGLE_RECAPTCHA_SITE_KEY') }}', {action: 'submit'})
+                    .then(function(token) {
+                        @this.emitSelf('setGoogleRecaptchaToken', token);
+                    });
+                });
+            });
+        });
+    </script>
+</div>
