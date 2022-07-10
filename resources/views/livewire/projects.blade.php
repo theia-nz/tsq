4d882aa96ac63e3ad0134edfd8ed7b26c00addc8
Kevin Lim <zzkevinlim@gmail.com>
2022-07-10 19:15:36 +1200

    202207101915

--- resources/views/livewire/projects.blade.php
+++ resources/views/livewire/projects.blade.php
@@ -2,13 +2,13 @@
     <div class="container mx-auto px-4">
         <div class="grid grid-cols-12 gap-y-[30px] lg:gap-y-[60px]">
             @foreach($projects as $project)
-            <div class="lg:col-start-1 col-span-12 lg:col-span-3 text-left mb-[30px] lg:mb-0 lg:text-right">
+            <div class="lg:col-start-1 col-span-12 lg:col-span-3 text-left lg:text-right">
                 <div class="font-primary text-[24px] text-primary-grey">{{ $project->title }}</div>
                 <div class="font-primary font-medium text-[20px] text-secondary-grey">{{ $project->description }}</div>
             </div>
             <div class="lg:col-start-6 col-span-12 lg:col-span-7 mb-[60px] lg:mb-0 relative group">
                 <div class="absolute inset-0 bg-white bg-opacity-0 transition-all group-hover:bg-opacity-50"></div>
-                <img class="object-contain object-center lg:object-left-top w-full h-[30vh]"
+                <img class="object-contain object-center lg:object-left-top w-full h-[30vh] bg-tertiary-grey-50 lg:bg-transparent"
                     src="{{ $project->image('featured_image') }}" alt="">
                 <a class="absolute inset-0" href="{{ route('project', $project->getSlug()) }}"></a>
             </div>
