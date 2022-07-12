3cb881c99b0dc97e0c61bc9fda01fb84e108a455
Kevin Lim <zzkevinlim@gmail.com>
2022-07-12 15:10:26 +1200

    202207121510 home page, make introduction screen load every single time

--- resources/views/pages/pageHome.blade.php
+++ resources/views/pages/pageHome.blade.php
@@ -5,8 +5,8 @@
 @section('content')
 
 <div class="flex items-center bg-primary-grey cursor-pointer absolute z-50 inset-0"
-	x-data="{ loadingScreen: $persist(true) }" x-cloak x-show="loadingScreen" x-transition:enter="transition-all"
-	x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
+	x-data="{ loadingScreen: @if(config('app.debug')) false @else true @endif }" x-cloak x-show="loadingScreen"
+	x-transition:enter="transition-all" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
 	x-transition:leave="transition-all duration-1000" x-transition:leave-start="opacity-100"
 	x-transition:leave-end="opacity-0" x-on:click="loadingScreen = false">
 	<img class="object-cover object-center w-full" src="{{ $repo->image('image_a') }}" alt="">
